eaa03104cd90c87786b385c48d56071c08596ae5
254njoki <njokifidelis12@gmail.com>
2025-09-13 12:30:51 +0300

    Add files via upload

---
+++ progam_displaying_unit.cpp
@@ -0,0 +1,21 @@
+//simple c program (Hello world) single line comment
+/*
+Name: Fidelis
+Reg: CT100/G/26151/25
+Description: Hello world program
+*/
+#include <stdio.h>//pre-processor descriptive.scanf
+
+//main function
+int main () {
+    printf("Introduction to programming and algorithms:\n");
+    printf("Introduction to computer applications:\n");
+    printf("Computer systems and organizations:\n");
+    printf("Analogue electronics:\n");
+    printf("Essentials of accounting:\n");
+    printf("Mathematics for science:\n");
+    printf("Fundamentals of ICT:\n");
+    printf("Communication and information for literacy skills:\n");
+    return 0;
+    }
+
